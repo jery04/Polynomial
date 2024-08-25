e95c065b2072fb387bf1fca64f0073b33e930a27
Jery <jery.rf03@gmail.com>
2024-08-25 04:46:43 -0400

    Polynomial version_1.0

---
+++ Ceros Polinomios/Form1.Designer.cs
@@ -0,0 +1,445 @@
+﻿namespace Ceros_Polinomios
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
+            label5 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            pictureBox1 = new PictureBox();
+            label19 = new Label();
+            label20 = new Label();
+            textBox4 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.ForeColor = Color.Transparent;
+            button1.Location = new Point(218, 176);
+            button1.Name = "button1";
+            button1.Size = new Size(212, 76);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.White;
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            textBox1.Location = new Point(158, 89);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(39, 27);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "a";
+            textBox1.Click += textBox1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Cursor = Cursors.IBeam;
+            textBox2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(255, 88);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(39, 27);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "b";
+            textBox2.Click += textBox2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Cursor = Cursors.IBeam;
+            textBox3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            textBox3.Location = new Point(352, 88);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(39, 27);
+            textBox3.TabIndex = 1;
+            textBox3.Text = "c";
+            textBox3.Click += textBox3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(294, 85);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 28);
+            label1.TabIndex = 2;
+            label1.Text = "x² +";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(199, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 28);
+            label2.TabIndex = 2;
+            label2.Text = "x³ +";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(394, 85);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 28);
+            label3.TabIndex = 2;
+            label3.Text = "x +";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(216, 136);
+            label4.Name = "label4";
+            label4.Size = new Size(80, 25);
+            label4.TabIndex = 3;
+            label4.Text = "Interval:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(304, 126);
+            label6.Name = "label6";
+            label6.Size = new Size(26, 38);
+            label6.TabIndex = 2;
+            label6.Text = "[";
+            // 
+            // textBox5
+            // 
+            textBox5.Cursor = Cursors.IBeam;
+            textBox5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(327, 135);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(33, 27);
+            textBox5.TabIndex = 1;
+            textBox5.Text = "i₁";
+            textBox5.Click += textBox5_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(361, 126);
+            label7.Name = "label7";
+            label7.Size = new Size(24, 38);
+            label7.TabIndex = 2;
+            label7.Text = ";";
+            // 
+            // textBox6
+            // 
+            textBox6.Cursor = Cursors.IBeam;
+            textBox6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox6.Location = new Point(381, 135);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(33, 27);
+            textBox6.TabIndex = 1;
+            textBox6.Text = "i₂";
+            textBox6.Click += textBox6_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI Semibold", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(415, 126);
+            label5.Name = "label5";
+            label5.Size = new Size(26, 38);
+            label5.TabIndex = 2;
+            label5.Text = "]";
+            // 
+            // label8
+            // 
+            label8.Location = new Point(0, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(100, 23);
+            label8.TabIndex = 8;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(233, 271);
+            label9.Name = "label9";
+            label9.Size = new Size(58, 23);
+            label9.TabIndex = 5;
+            label9.Text = "Roots:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(233, 303);
+            label10.Name = "label10";
+            label10.Size = new Size(47, 23);
+            label10.TabIndex = 5;
+            label10.Text = "x₁ = ";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(233, 330);
+            label11.Name = "label11";
+            label11.Size = new Size(43, 23);
+            label11.TabIndex = 5;
+            label11.Text = "x₂ =";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.Transparent;
+            label12.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.Location = new Point(233, 356);
+            label12.Name = "label12";
+            label12.Size = new Size(43, 23);
+            label12.TabIndex = 5;
+            label12.Text = "x₃ =";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.Transparent;
+            label13.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.Location = new Point(352, 271);
+            label13.Name = "label13";
+            label13.Size = new Size(67, 46);
+            label13.TabIndex = 5;
+            label13.Text = "Critical \r\nPoints:";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.BackColor = Color.Transparent;
+            label14.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.Location = new Point(358, 329);
+            label14.Name = "label14";
+            label14.Size = new Size(47, 23);
+            label14.TabIndex = 5;
+            label14.Text = "x₁ = ";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.BackColor = Color.Transparent;
+            label15.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label15.Location = new Point(358, 356);
+            label15.Name = "label15";
+            label15.Size = new Size(48, 23);
+            label15.TabIndex = 5;
+            label15.Text = "x₂ = ";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.BackColor = Color.Transparent;
+            label16.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label16.Location = new Point(274, 303);
+            label16.Name = "label16";
+            label16.Size = new Size(15, 23);
+            label16.TabIndex = 5;
+            label16.Text = " ";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.BackColor = Color.Transparent;
+            label17.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label17.Location = new Point(274, 330);
+            label17.Name = "label17";
+            label17.Size = new Size(0, 23);
+            label17.TabIndex = 5;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.BackColor = Color.Transparent;
+            label18.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label18.Location = new Point(274, 356);
+            label18.Name = "label18";
+            label18.Size = new Size(0, 23);
+            label18.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(158, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(323, 62);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.BackColor = Color.Transparent;
+            label19.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label19.Location = new Point(402, 330);
+            label19.Name = "label19";
+            label19.Size = new Size(15, 23);
+            label19.TabIndex = 5;
+            label19.Text = " ";
+            // 
+            // label20
+            // 
+            label20.AutoSize = true;
+            label20.BackColor = Color.Transparent;
+            label20.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label20.Location = new Point(402, 357);
+            label20.Name = "label20";
+            label20.Size = new Size(15, 23);
+            label20.TabIndex = 5;
+            label20.Text = " ";
+            // 
+            // textBox4
+            // 
+            textBox4.Cursor = Cursors.IBeam;
+            textBox4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(442, 89);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(39, 27);
+            textBox4.TabIndex = 7;
+            textBox4.Text = "d";
+            textBox4.Click += textBox4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(663, 393);
+            Controls.Add(textBox4);
+            Controls.Add(pictureBox1);
+            Controls.Add(label12);
+            Controls.Add(label15);
+            Controls.Add(label11);
+            Controls.Add(label14);
+            Controls.Add(label18);
+            Controls.Add(label17);
+            Controls.Add(label20);
+            Controls.Add(label19);
+            Controls.Add(label16);
+            Controls.Add(label10);
+            Controls.Add(label13);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Polynomial";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label6;
+        private TextBox textBox5;
+        private Label label7;
+        private TextBox textBox6;
+        private Label label5;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+        private PictureBox pictureBox1;
+        private Label label19;
+        private Label label20;
+        private TextBox textBox4;
+    }
+}
---
+++ Ceros Polinomios/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Ceros_Polinomios
+{
+
+    public partial class Form1 : Form
+    {
+        // Properties
+        TextBox[] Coeff {  get; set; }
+        TextBox[] Interval { get; set; }
+        List<double> Zeros { get; set; }
+        List<double> Critical_points {  get; set; }
+        Label[] Zeros_labels {  get; set; }
+        Label[] First_order_labels { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Coeff = new TextBox[4]
+            {
+                textBox1, textBox2, textBox3, textBox4,
+            };
+            this.Interval = new TextBox[2]
+            {
+                textBox5, textBox6
+            };
+            this.Zeros_labels = new Label[3]
+            {
+                label16, label17, label18
+            };
+            this.First_order_labels = new Label[2]
+            {
+                label19, label20
+            };
+            this.Critical_points = new List<double>();
+            this.Zeros = new List<double>();
+        }
+
+        // Methods
+        public double Evaluar(double[] poly, double num)
+        {
+            double result = 0;
+            for (int i = 0; i < poly.Length; i++)
+                result += (double)poly[i] * Math.Pow(num, poly.Length - 1 - i);
+
+            return result;
+        }
+        public void Bisec(double[] poly, double a, double b, List<double> zero)
+        {
+            if (b - a < 0000.1)
+            {
+                double zeroCurrent = Math.Round((a + b) / 2, 4);
+                if (Math.Round(zeroCurrent) - zeroCurrent < 0.1)
+                    zero.Add(Math.Round(zeroCurrent));
+                else
+                    zero.Add(Math.Round(zeroCurrent, 4));
+            }
+
+            else
+            {
+                double medium = Math.Round((a + b) / 2, 5);
+
+                if (Bolzano(poly, a, medium))
+                    Bisec(poly, a, medium, zero);
+
+                if (Bolzano(poly, medium, b))
+                    Bisec(poly, medium, b, zero);
+            }
+        }
+        public bool Bolzano(double[] pol, double inf, double sup)
+        {
+            double a_evaluated = Evaluar(pol, inf);
+            double half = (inf + sup) / 2;
+            double halfCurrent = half;
+
+            while (sup >= half || halfCurrent >= inf)
+            {
+                if (sup >= inf)
+                {
+                    if (a_evaluated * Evaluar(pol, sup) <= 0)
+                        return true;
+                    else
+                        sup -= 0.00002;
+                }
+
+                if (halfCurrent >= inf)
+                {
+                    if (a_evaluated * Evaluar(pol, halfCurrent) <= 0)
+                        return true;
+                    else
+                        halfCurrent -= 0.00002;
+                }
+            }
+            return false;
+        }
+        public bool CheckInput(TextBox[] coeff, TextBox[] interval)
+        {
+            bool check = true;
+
+            foreach (TextBox input in coeff)
+            {
+                if (!IsDigit(input.Text))
+                { input.BackColor = Color.Red; check = false; }
+                else 
+                    input.BackColor = Color.White;
+            }
+            foreach (TextBox input in interval)
+            {
+                if (!IsDigit(input.Text))
+                { input.BackColor = Color.Red; check = false; }
+                else 
+                    input.BackColor = Color.White;
+            }
+            return check;
+        }
+        public bool IsDigit(string num)
+        {
+            if (string.IsNullOrWhiteSpace(num))
+                return false;
+
+            return num.All(c => c == '-' || Char.IsDigit(c));
+        }
+        public double[] GetPoly(TextBox[] coeff)
+        {
+            double[] poly = new double[4];
+
+            for (int i = 0; i < coeff.Length; i++)
+                poly[i] = Convert.ToDouble(coeff[i].Text);
+
+            return poly;
+        }
+        public void PrintZeros()
+        {
+            for (int i = 0; i < Zeros.Count; i++)
+                Zeros_labels[i].Text = Convert.ToString(Zeros[i]);
+        }
+        public void PrintCriticalPoints(double[] first_order)
+        {
+            for (int i = 0; i < Critical_points.Count; i++) 
+                First_order_labels[i].Text = Convert.ToString(Critical_points[i]) +" "+ MaxOrMin(first_order, Critical_points[i]);
+        }
+        public double[] GetFirstOrder(double[] poly)
+        {
+            double[] first_order = new double[4];
+            first_order[0] = 0;
+
+            for (int i = 0; i < poly.Length - 1; i++)
+                first_order[i + 1] = poly[i] * (poly.Length - (1+i));
+
+            return first_order;
+        } 
+        public string MaxOrMin(double[] first_order, double x)
+        {
+            double left = Evaluar(first_order, x - 0.5);
+            double right = Evaluar(first_order, x + 0.5);
+
+            if (left > 0 && right < 0)
+                return "(Max)";
+
+            else if (left < 0 && right > 0)
+                return "(Min)";
+
+            else if (left * right > 0)
+                return "(Const)";
+
+            return "";
+        }
+        public void ClearEverything()
+        {
+            Zeros.Clear();
+            Critical_points.Clear();
+
+            foreach (Label item in Zeros_labels)
+                item.Text = "";
+
+            foreach (Label item in First_order_labels)
+                item.Text = "";
+        }
+
+        // Botton_Calculate
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (CheckInput(Coeff, Interval))
+            {
+                double intvl1 = Convert.ToDouble(Interval[0].Text);
+                double intvl2 = Convert.ToDouble(Interval[1].Text);
+                double[] poly = GetPoly(Coeff);
+                double[] first_order = GetFirstOrder(poly);
+
+                Bisec(poly, intvl1, intvl2, Zeros);
+                Bisec(first_order, intvl1, intvl2, Critical_points);
+
+                PrintZeros();
+                PrintCriticalPoints(first_order);
+            }
+        }
+
+        // Labels
+        // Click
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear(); ClearEverything();
+        }
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear(); ClearEverything();
+        }
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear(); ClearEverything();
+        }
+        private void textBox4_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear(); ClearEverything();
+        }
+        private void textBox5_Click(object sender, EventArgs e)
+        {
+            textBox5.Clear(); ClearEverything();
+        }
+        private void textBox6_Click(object sender, EventArgs e)
+        {
+            textBox6.Clear(); ClearEverything();
+        }
+    }
+}
